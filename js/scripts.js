f777a0714355955affd8af0e3678fe34222bfd78
marsfrolova <115263855+marsfrolova@users.noreply.github.com>
2022-11-03 20:56:40 +0200

    f

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel();
   });
 
-  $('.owl-carousel').owlCarousel({
+  $(".owl-carousel").owlCarousel({
     loop: true, 
     margin:0, 
     nav:true, 
